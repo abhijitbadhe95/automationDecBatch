6bc9eb32a72580b2a679a8f6c1e772f9337f07b9
abhijitbadhe95 <100512745+abhijitbadhe95@users.noreply.github.com>
2022-02-27 15:46:42 +0530

    Add files via upload

---
+++ Configurations.java
@@ -0,0 +1,16 @@
+package com.vel.config;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class Configurations  {
+	public static String driverPath="C:\\Users\\I-Net\\Downloads\\chromedriver_win32\\chromedriver.exe";
+	public static String
+	appURL="https://www.facebook.com/signup/";
+	
+	public static String excelSheetPath = "E:\\Velocity\\Setup\\Book1.xlsx";
+	//String username= path.getRow(0).getCell(0).getStringCellValue();
+	//System.out.println(username);
+}
